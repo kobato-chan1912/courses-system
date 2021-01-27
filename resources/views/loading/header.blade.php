801a01ce42471605ae3378d2380c769c5cf9236f
MAI VIẾT DŨNG <no_body156@yahoo.com.vn>
2021-01-27 21:09:45 +0700

    first init

---
+++ resources/views/loading/header.blade.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=9">
+    <meta name="description" content="Gambolthemes">
+    <meta name="author" content="Gambolthemes">
+    <title>{{$title}}</title>
+
+    <!-- Favicon Icon -->
+    <link rel="icon" type="image/png" href="images/fav.png">
+
+    <!-- Stylesheets -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet'>
+    <link href='{{asset("vendor/unicons-2.0.1/css/unicons.css")}}' rel='stylesheet'>
+    <link href="{{asset("css/vertical-responsive-menu.min.css")}}" rel="stylesheet">
+    <link href="{{asset("css/style.css")}}" rel="stylesheet">
+    <link href="{{asset("css/responsive.css")}}" rel="stylesheet">
+    <link href="{{asset("css/night-mode.css")}}" rel="stylesheet">
+
+    <!-- Vendor Stylesheets -->
+    <link href="{{asset("vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet">
+    <link href="{{asset("vendor/OwlCarousel/assets/owl.carousel.css")}}" rel="stylesheet">
+    <link href="{{asset("vendor/OwlCarousel/assets/owl.theme.default.min.css")}}" rel="stylesheet">
+    <link href="{{asset("vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/semantic/semantic.min.css")}}">
+
+</head>
---
+++ resources/views/loading/js.blade.php
@@ -0,0 +1,7 @@
+<script src="{{asset("js/vertical-responsive-menu.min.js")}}"></script>
+<script src="{{asset("js/jquery-3.3.1.min.js")}}"></script>
+<script src="{{asset("vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{asset("vendor/OwlCarousel/owl.carousel.js")}}"></script>
+<script src="{{asset("vendor/semantic/semantic.min.js")}}"></script>
+<script src="{{asset("js/custom.js")}}"></script>
+<script src="{{asset("js/night-mode.js")}}"></script>
